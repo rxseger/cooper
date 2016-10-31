3acbaaf9d2ef9a0702bf45d92f879e56f69fd8f2
rxseger <rseger@gmx.co.uk>
2016-10-30 17:45:44 -0700

    Set a flag when receiving an interrupt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import ubinascii
 import webrepl
 import micropython
+
 micropython.alloc_emergency_exception_buf(100)
 
 from umqtt.simple import MQTTClient
@@ -56,11 +57,12 @@ def save_config():
     except OSError:
         print("Couldn't save /config.json")
 
-global gpio_changed
-gpio_changed = None
+global gpio_changed, any_gpio_changed
+gpio_changed = bytearray(16) # sixteen bytes (position=pin #), 0=not changed, 1=changed
+any_gpio_changed = False
 
 def main():
-    global gpio_changed
+    global gpio_changed, any_gpio_changed
     load_config()
     analog_pin = machine.ADC(0) # pin A0 on ESP8266
     client = MQTTClient(CONFIG['client_id'], CONFIG['broker'])
@@ -69,16 +71,28 @@ def main():
 
     # interrupt-driven switches
     print('Switches:',CONFIG['switches'])
-    pins = {} # name to object
+    name2pin = {}
+    pin_number2name = {}
     for info in CONFIG['switches']:
-        pin = machine.Pin(info['pin'], machine.Pin.IN, info['pull_up_down'])
-        pins[info['name']] = pin
-        def handler(what):
-            global gpio_changed
-            gpio_changed = what # this is about all we can do in an ISR # TODO: each pin independently
-            #print('read {}: {}'.format(pin,pin.value()))
-        print('Watching pin {} = {}'.format(info['pin'], info['name']))
-        pin.irq(trigger=info['trigger'], handler=handler)
+        pin_number = info['pin']
+        name = info['name']
+
+        pin = machine.Pin(pin_number, machine.Pin.IN, info['pull_up_down'])
+        name2pin[name] = pin
+        pin_number2name[pin_number] = name
+
+        def add_handler(pin_number, name, trigger):
+            def handler(ignored): # ISR argument is a Pin object, which can't convert back to a pin number?! Ignore it and use closure
+                print('pin change {}',pin_number)
+                global gpio_changed, any_gpio_changed
+                gpio_changed[pin_number] = 1 # flag this pin as having changed
+                any_gpio_changed = True
+                # important: can't do much in an ISR, see https://micropython.org/resources/docs/en/latest/wipy/reference/isr_rules.html
+                #print('read {}: {}'.format(pin,pin.value()))
+            print('Watching pin {} = {}'.format(pin_number, name))
+            pin.irq(trigger=trigger, handler=handler)
+
+        add_handler(pin_number, name, info['trigger'])
 
     # poll
     while True:
@@ -87,11 +101,29 @@ def main():
                                           CONFIG['client_id']),
                                           bytes(str(data), 'utf-8'))
         print('Sensor state: {}'.format(data))
-        if gpio_changed is not None:
-            print('GPIO pin changed: {}'.format(gpio_changed))
-            for name, pin in pins.items():
-                print('Pin {}: {}'.format(pin, pin.value()))
-            gpio_changed = None
+
+        if any_gpio_changed:
+            print('Saw some GPIO change:',gpio_changed)
+            # Critical section: accumulate list of changed GPIO pins
+            list_gpio_changed = []
+            #pyb.disable_irq() # note: not pyb as in https://micropython.org/resources/docs/en/latest/wipy/reference/isr_rules.html - see https://github.com/micropython/micropython/issues/2085
+            #machine.disable_irq() # TODO: why does this seem to hang the program over the console, even though I re-enable interrupts?
+            for pin, flag in enumerate(gpio_changed):
+                if flag:
+                    list_gpio_changed.append(pin)
+            any_gpio_changed = False
+            gpio_changed = bytearray(16)
+            #machine.enable_irq()
+
+            # Get what it changed to
+            for pin_number in list_gpio_changed:
+                name = pin_number2name[pin_number]
+                pin = name2pin[name]
+
+                value = pin.value()
+            
+                print('GPIO pin changed: {} -> {}'.format(name, value))
+
 
         time.sleep(1) # TODO: configurable interval
 
