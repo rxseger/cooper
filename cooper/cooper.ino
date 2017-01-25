60e46b193183721644b0f8b38a4dbef7fabe4c91
rxseger <rseger@gmx.co.uk>
2017-01-24 20:23:43 -0800

    Simply the 404 not found response

--- cooper/cooper.ino
+++ cooper/cooper.ino
@@ -62,18 +62,10 @@ void handleRoot() {
 
 void handleNotFound(){
   digitalWrite(led, 0);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
+  String message = "Not found: " + server.uri(); + "\n\n";
+
   server.send(404, "text/plain", message);
+
   digitalWrite(led, 1);
 }
 
